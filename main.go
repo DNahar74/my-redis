715c34e2e9d2f265fbb3b62ed360cefe82288cd2
DNahar74 <nahardarsh55@gmail.com>
2025-03-21 18:18:46 +0530

    added a basic ping/pong and client connection

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
+//! (1) When a client disconnects it shows :: {Error reading from connection:  EOF}
+//TODO: (1) Allow multiple clients to connect
+
 package main
 
 import (
 	"fmt"
 	"net"
+	"strings"
 )
 
 func main() {
@@ -16,4 +20,44 @@ func main() {
 	defer listener.Close()
 
 	fmt.Println("Server listening on port 6379")
+
+	conn, err := listener.Accept()
+	if err != nil {
+		fmt.Println("Error connecting the client", err)
+		return
+	}
+
+	fmt.Println("Client connected")
+
+	buf := make([]byte, 128)
+
+	for {
+		_, err = conn.Read(buf)
+		if err != nil {
+			fmt.Println("Error reading from connection: ", err.Error())
+			return
+		}
+		defer conn.Close()
+	
+		fmt.Printf("read command:\n%s", buf)
+	
+		commands := getCommands(buf)
+	
+		if commands[2] == "PING" {
+			message := []byte("+PONG\r\n")
+			_, err := conn.Write(message)
+			if err != nil {
+				fmt.Printf("Error writing to client: %v\n", err)
+			}
+		}
+	}
+}
+
+func getCommands(buf []byte) []string {
+	val := string(buf)
+	val = strings.ToUpper(val)
+	
+	inputs := strings.Split(val, "\r\n")
+
+	return inputs
 }
