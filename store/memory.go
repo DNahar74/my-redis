19f195bcdc032d6da40c007a3f4d21e79bdf2871
DNahar74 <nahardarsh55@gmail.com>
2025-04-05 04:09:55 +0530

    added a basic storage

---
+++ store/memory.go
@@ -0,0 +1,27 @@
+package store
+
+import (
+	"time"
+
+	"github.com/DNahar74/my-redis/resp"
+)
+
+// Data represents a key-value pair in the store
+type Data struct {
+	Value  resp.RESPType
+	Expiry time.Time
+}
+
+// Store is a map of keys to Data items
+type Store struct {
+	Items map[string]Data
+}
+
+// CreateStorage initializes a new store instance
+func CreateStorage() *Store {
+	s := &Store{
+		Items: make(map[string]Data),
+	}
+
+	return s
+}
